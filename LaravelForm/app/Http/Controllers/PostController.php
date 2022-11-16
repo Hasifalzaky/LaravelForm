48b00657dd4e9284ddcdd1e6c65ebc814f32541e
Hasif al zaky <hasippo12327@gmail.com>
2022-11-16 19:04:04 +0700

    update crud

--- LaravelForm/app/Http/Controllers/PostController.php
+++ LaravelForm/app/Http/Controllers/PostController.php
@@ -4,16 +4,41 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 class PostController extends Controller
 {
-    public function index()
-    {
-        return view('add-blog-post-form');
-    }
     public function store(Request $request)
     {
         $post = new Post;
         $post->title = $request->title;
         $post->description = $request->description;
         $post->save();
-        return redirect('add-blog-post-form')->with('status', 'Blog Post Form Data Has Been inserted');
+        return redirect('/read')->with('status', 'Blog Post Form Data Has Been inserted');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Post::find($id);
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->update();
+        return redirect('/read')->with('status', 'Blog Post Form Data Has Been updated');
+    }
+
+    public function read() 
+    {  
+        $model_post= new Post;
+        $data=$model_post->all();
+        return view('read',['data' =>$data]); 
+    }
+
+    public function edit($id)
+    {   
+        $data = Post::findOrFail($id);
+        return view('edit', ['data' =>$data]);
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/read')->with('status', 'Blog Post Form Data Has Been deleted');
     }
 }
